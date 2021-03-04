f72717771b00f47c877467072b209504c6ea3be9
boring-km <kms0644804@naver.com>
2021-03-04 12:15:29 +0900

    [master] 다양한 함수형 인터페이스를 사용해보자

--- test/FunctionalInterfaceTest.java
+++ test/FunctionalInterfaceTest.java
@@ -2,11 +2,13 @@ import basic.Calculation;
 import basic.FpCalculationService;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.OptionalDouble;
-import java.util.function.Function;
+import java.util.function.*;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class FunctionalInterfaceTest {
     @Test
@@ -35,5 +37,35 @@ public class FunctionalInterfaceTest {
         int chooseMyselfResult = chooseMyself.apply(100);
         assertEquals(expected, identityResult);
         assertEquals(expected, chooseMyselfResult);
+    }
+
+    @Test
+    public void Predicate_인터페이스로_test_하려는_값의_조건을_판단하여_boolean_형태로_반환한다() {
+        final Predicate<Integer> isPositive = i -> i > 0;
+        assertTrue(isPositive.test(10));
+        assertFalse(isPositive.test(-100));
+
+        final BiPredicate<String, String> isLonger = (s1, s2) -> s1.length() > s2.length();
+        assertTrue(isLonger.test("1234567", "12345"));
+        assertFalse(isLonger.test("나는 진강민이다", "정말로 정신을 차리고 다시 기본을 다져야한다."));
+    }
+
+    @Test
+    public void Consumer는_리턴이_없는_형태로_추상메서드_accept을_사용한다() {
+        final Consumer<String> print = System.out::println;
+        print.accept("나는");
+        print.accept("절박하게");
+        print.accept("공부를 한다.");
+
+        final Consumer<String> printTime = System.out::println;
+        final Consumer<String> mergedPrint = printTime.andThen(print);
+        mergedPrint.accept(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
+    }
+
+    @Test
+    public void Supplier는_파라미터가_없는_형태이며_추상메서드_get을_사용한다() {
+        final Supplier<String> supplier = () -> "정신을 똑바로 차리자";
+        String result = supplier.get();
+        assertEquals("정신을 똑바로 차리자", result);
     }
 }
