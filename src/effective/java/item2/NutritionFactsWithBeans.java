f06e62e94b40ec09695a54847100cd611a385d02
boring-km <kms0644804@naver.com>
2021-04-03 23:06:00 +0900

    아이템2. setter()를 사용해서 초기화하다보면 일관성이 무너진다

---
+++ src/effective/java/item2/NutritionFactsWithBeans.java
@@ -0,0 +1,36 @@
+package effective.java.item2;
+
+public class NutritionFactsWithBeans {
+    private int servingSize = -1;
+    private int servings = -1;
+    private int calories = 0;
+    private int fat = 0;
+    private int sodium = 0;
+    private int carbohydrate = 0;
+
+    public NutritionFactsWithBeans() { } // 책에 적혀는 있지만 의미는 없어보인다.
+
+    public void setServingSize(int servingSize) {
+        this.servingSize = servingSize;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public void setSodium(int sodium) {
+        this.sodium = sodium;
+    }
+
+    public void setCarbohydrate(int carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+}
