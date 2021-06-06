f237cf687b221e48bfc97212034c3adeceaa8342
boring-km <kms0644804@naver.com>
2021-06-06 20:12:09 +0900

    아이템11. hashCode()를 재정의하고 다시 비교하기

--- src/effective/java/hashcode/Holiday.java
+++ src/effective/java/hashcode/Holiday.java
@@ -1,5 +1,7 @@
 package effective.java.hashcode;
 
+import java.util.Objects;
+
 public class Holiday {
     private int month;
     private int day;
@@ -7,5 +9,21 @@ public class Holiday {
     public Holiday(int month, int day) {
         this.month = month;
         this.day = day;
+    }
+
+    // 자동 생성된 equals() 사용함
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Holiday holiday = (Holiday) o;
+        return month == holiday.month && day == holiday.day;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Integer.hashCode(month);
+        result = 31 * result + Integer.hashCode(day);
+        return result;
     }
 }
--- test/effective/java/hashcode/HolidayTest.java
+++ test/effective/java/hashcode/HolidayTest.java
@@ -9,13 +9,13 @@ import static org.junit.Assert.*;
 
 public class HolidayTest {
     @Test
-    public void hashCode를_재정의하지_않고_2개의_동일한_인스턴스를_비교하면_같지_않다() {
+    public void hashCode를_정상적으로_재정의하고_2개의_동일한_필드를_가진_인스턴스를_비교하면_같다() {
         Map<Holiday, String> holidayMap = new HashMap<>();
         String expected = "삼일절";
         holidayMap.put(new Holiday(3, 1), expected);
 
         String actual = holidayMap.get(new Holiday(3, 1));
 
-        assertNotEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 }
